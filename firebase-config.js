dc2b0905f8294ffd9e9efc701759b2332182ce96
longrothna <borgesmatthewj@gmail.com>
2025-12-26 05:58:25 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,1 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBE4iy1F8SUjpKcv3tpIi3VE5pixvxIaTk",
-  authDomain: "b0-pro.firebaseapp.com",
-  projectId: "b0-pro",
-  storageBucket: "b0-pro.firebasestorage.app",
-  messagingSenderId: "365201811616",
-  appId: "1:365201811616:web:564942962a802dac71c1c5",
-  measurementId: "G-V3E11TYNKD",
-};
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
